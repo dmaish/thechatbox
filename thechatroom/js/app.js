ee8de9e8fb79dbfce20f1db56ac903791c0eb0b7
dmaish <mainadaniel81@gmail.com>
2016-07-21 09:55:05 +0300

    fifth commit

--- thechatroom/js/app.js
+++ thechatroom/js/app.js
@@ -1,3 +1,10 @@
 $(document).foundation()
+function validateForm() {
+    var x = document.forms["myform"]["fname"].value;
+    if (x == null || x == "") {
+        alert("The email section must be filled out");
+        return false;
+    }
+}
 
 
